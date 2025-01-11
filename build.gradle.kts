39f82135a38682f102602ca66f76c38b5fbe623b
damiankaplonspa <damian.kaplon@sportalliance.com>
2025-01-11 15:34:56 +0100

    Add exposed gradle dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 val kotlin_version: String by project
 val logback_version: String by project
-val flyway_version: String by project
+val postgresql_version: String by project
+val exposed_version: String by project
 
 plugins {
-    kotlin("jvm") version "2.1.0"
-    kotlin("plugin.serialization") version "2.1.0"
-    id("io.ktor.plugin") version "3.0.1"
-    id("org.flywaydb.flyway") version "11.1.1"
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    id("io.ktor.plugin")
+    id("org.flywaydb.flyway")
 }
 
 group = "io.cruxvelo.fellowup"
@@ -15,8 +16,10 @@ version = "0.0.1"
 
 buildscript {
     dependencies {
-        classpath("org.flywaydb:flyway-database-postgresql:11.1.1")
-        classpath("org.postgresql:postgresql:42.7.4")
+        val flyway_version: String by project
+        classpath("org.flywaydb:flyway-database-postgresql:$flyway_version")
+        val postgresql_version: String by project
+        classpath("org.postgresql:postgresql:$postgresql_version")
     }
 }
 
@@ -33,7 +36,6 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml-jvm")
     implementation("io.ktor:ktor-server-auth-jwt-jvm")
     implementation("io.ktor:ktor-server-auth-jvm")
@@ -41,6 +43,10 @@ dependencies {
     implementation("io.ktor:ktor-client-apache-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("org.postgresql:postgresql:$postgresql_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
 
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
