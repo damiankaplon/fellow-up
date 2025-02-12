0e3ffc5eb0a77e4112f537157603c13aa8b0ffdf
damiankaplonspa <damian.kaplon@sportalliance.com>
2025-02-12 20:43:00 +0100

    Close the dialog on submit

--- web/fellow-up-ui/src/matchmaking/Matchmakings.tsx
+++ web/fellow-up-ui/src/matchmaking/Matchmakings.tsx
@@ -2,13 +2,46 @@ import {IconButton, Typography} from "@mui/material";
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import {MatchmakingWizardDialog} from "./wizard/MatchmakingWizardDialog.tsx";
 import React from "react";
+import {MatchmakingWizardResult} from "./wizard/MatchmakingWizard.tsx";
+import {FellowUpAuthContext} from "../security/FellowUpAuthContext.tsx";
+
+interface CreateMatchmakingBody {
+  category: string;
+  at: string;
+}
+
+function createRequestBody(from: MatchmakingWizardResult): CreateMatchmakingBody {
+  return {
+    category: from.category,
+    at: from.date.toISOString()
+  };
+}
+
+async function createMatchmaking(
+  jwt: string,
+  result: MatchmakingWizardResult
+) {
+  await fetch(
+    '/api/matchmakings',
+    {
+      method: 'POST',
+      body: JSON.stringify(createRequestBody(result)),
+      headers: {'Authorization': `Bearer ${jwt}`, 'Content-Type': 'application/json', 'accept': 'application/json'}
+    }
+  );
+}
 
 export default function Matchmakings() {
   const [showMatchmakingWizard, setShowMatchmakingWizard] = React.useState(false);
+  const authContext = React.useContext(FellowUpAuthContext);
   return (
     <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
       <Typography variant={"h6"}>Matchmakings</Typography>
       <MatchmakingWizardDialog open={showMatchmakingWizard}
+                               onComplete={(result: MatchmakingWizardResult) =>
+                                 createMatchmaking(authContext.jwt!, result)
+                                   .then(() => setShowMatchmakingWizard(false))
+                               }
                                onClose={() => setShowMatchmakingWizard(false)}/>
       <IconButton onClick={() => setShowMatchmakingWizard(!showMatchmakingWizard)}>
         <AddBoxIcon fontSize="large" color="primary"/>
