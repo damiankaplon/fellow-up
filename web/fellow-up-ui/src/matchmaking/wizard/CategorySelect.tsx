0cd9d5cb332d849e9f3c4a9fb425d3d5d41a2130
damiankaplonspa <damian.kaplon@sportalliance.com>
2025-02-12 20:57:55 +0100

    Add category input validation

--- web/fellow-up-ui/src/matchmaking/wizard/CategorySelect.tsx
+++ web/fellow-up-ui/src/matchmaking/wizard/CategorySelect.tsx
@@ -3,22 +3,35 @@ import React, {ChangeEvent} from "react";
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export interface CategorySelectProps {
-    onCategorySelect: (category: string) => void;
+  onCategorySelect: (category: string) => void;
 }
 
+const inputValidator = (input: string) => input.length > 0;
+
 export function CategorySelect(props: CategorySelectProps) {
-    const [category, setCategory] = React.useState('');
-    const confirmButton = (
-        <IconButton onClick={() => props.onCategorySelect(category)}>
-            <NavigateNextIcon/>
-        </IconButton>);
-    return (
-        <TextField id="standard-basic"
-                   label="Category"
-                   onChange={(event: ChangeEvent<HTMLInputElement>) => setCategory(event.target.value)}
-                   variant="standard"
-                   slotProps={{input: {endAdornment: confirmButton}}}
-        />
-    );
+  const [category, setCategory] = React.useState('');
+  const [isValid, setIsValid] = React.useState(true);
+  const confirmButton = (
+    <IconButton onClick={() => {
+      if (isValid) {
+        props.onCategorySelect(category);
+      }
+    }}>
+      <NavigateNextIcon/>
+    </IconButton>);
+  return (
+    <TextField
+      error={!isValid}
+      helperText={isValid ? '' : 'Category must not be empty'}
+      label="Category"
+      onChange={(event: ChangeEvent<HTMLInputElement>) => {
+        setCategory(event.target.value);
+        const isValid = inputValidator(event.target.value);
+        setIsValid(isValid);
+      }}
+      variant="standard"
+      slotProps={{input: {endAdornment: confirmButton}}}
+    />
+  );
 
 }
