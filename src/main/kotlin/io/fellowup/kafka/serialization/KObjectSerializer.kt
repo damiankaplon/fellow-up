c139e7efbcb2b1f623efbfc865c408a300b0e767
damiankaplonspa <damian.kaplon@sportalliance.com>
2025-03-22 22:47:35 +0100

    Adjust kafka packages

--- src/main/kotlin/io/fellowup/kafka/serialization/KObjectSerializer.kt
+++ src/main/kotlin/io/fellowup/kafka/serialization/KObjectSerializer.kt
@@ -1,8 +1,6 @@
-package io.fellowup.kafka
+package io.fellowup.kafka.serialization
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.apache.kafka.common.serialization.Serializer
-import org.apache.kafka.common.serialization.StringSerializer
 
 fun interface JsonMapper {
     fun toString(obj: Any): String
@@ -23,11 +21,3 @@ open class KObjectSerializer(
        return kafkaStringSerializer.serialize(topic, jsonString)
     }
 }
-
-private val objectMapper = jacksonObjectMapper()
-private val stringSerializer = StringSerializer()
-
-object KObjectMapperObjectSerializer : KObjectSerializer(
-    JsonMapper { objectMapper.writeValueAsString(it) },
-    KafkaStringSerializer { topic, data -> stringSerializer.serialize(topic, data) }
-)
