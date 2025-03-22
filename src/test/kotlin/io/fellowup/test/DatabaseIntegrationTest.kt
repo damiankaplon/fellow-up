78a2a11db8e5330778156251d9bdf3485e1a3006
damiankaplonspa <damian.kaplon@sportalliance.com>
2025-03-22 20:10:14 +0100

    Make `DatabaseIntegrationTest` rollback transaction after failures as well

--- src/test/kotlin/io/fellowup/test/DatabaseIntegrationTest.kt
+++ src/test/kotlin/io/fellowup/test/DatabaseIntegrationTest.kt
@@ -6,9 +6,17 @@ import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
 internal abstract class DatabaseIntegrationTest {
-    private val db = TestDatabaseConnectionProvider.provide()
+    protected val db = TestDatabaseConnectionProvider.provide()
 
-    protected fun test(test: suspend Transaction.() -> Unit) = runBlocking {
-        newSuspendedTransaction(Dispatchers.Default, db = db) { test(); rollback() }
+    protected fun test(test: suspend Transaction.() -> Unit): Unit = runBlocking {
+        newSuspendedTransaction(Dispatchers.Default, db = db) {
+            try {
+                test()
+            } catch (e: Throwable) {
+                throw e
+            } finally {
+                rollback()
+            }
+        }
     }
 }
