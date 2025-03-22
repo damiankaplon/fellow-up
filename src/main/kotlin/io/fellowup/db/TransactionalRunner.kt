a1616fffcf7a3aa24c1fd18fc301731025ef77f3
damiankaplonspa <damian.kaplon@sportalliance.com>
2025-03-22 20:09:36 +0100

    Make `ExposedTransactionalRunner` provide current transaction when there is a request for na new transaction when there is already ongoing transaction

--- src/main/kotlin/io/fellowup/db/TransactionalRunner.kt
+++ src/main/kotlin/io/fellowup/db/TransactionalRunner.kt
@@ -2,7 +2,10 @@ package io.fellowup.db
 
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.transactions.TransactionManager
+import org.jetbrains.exposed.sql.transactions.experimental.suspendedTransactionAsync
+import org.jetbrains.exposed.sql.transactions.experimental.withSuspendTransaction
 
 interface TransactionalRunner {
 
@@ -16,8 +19,16 @@ interface TransactionalRunner {
 class ExposedTransactionalRunner(
     private val database: Database
 ) : TransactionalRunner {
-    override suspend fun <T> transaction(isolation: Int, readOnly: Boolean, block: suspend () -> T): T =
-        newSuspendedTransaction(Dispatchers.IO, readOnly = readOnly, db = database) {
-            block()
+    override suspend fun <T> transaction(isolation: Int, readOnly: Boolean, block: suspend () -> T): T {
+        val current: Transaction? = TransactionManager.currentOrNull()
+        return if (current != null) {
+            current.withSuspendTransaction { block() }
+        } else {
+            suspendedTransactionAsync(
+                Dispatchers.IO,
+                db = database,
+                transactionIsolation = isolation
+            ) { block() }.await()
         }
+    }
 }
