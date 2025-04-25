9d188dee7b9d90d95c161493ef6da84454c18f28
damiankaplonspa <damian.kaplon@sportalliance.com>
2025-04-25 23:10:41 +0200

    Extract haversin formula

--- infrastructure/src/test/kotlin/io/fellowup/infrastructure/test/matchmaking/ActivityInMemoryRepository.kt
+++ infrastructure/src/test/kotlin/io/fellowup/infrastructure/test/matchmaking/ActivityInMemoryRepository.kt
@@ -1,14 +1,11 @@
 package io.fellowup.infrastructure.test.matchmaking
 
+import haversineDistance
 import io.fellowup.domain.matchmaking.Activity
 import io.fellowup.domain.matchmaking.ActivityRepository
 import io.fellowup.domain.matchmaking.Location
 import java.time.Instant
 import java.time.temporal.ChronoUnit
-import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.sin
-import kotlin.math.sqrt
 
 internal class ActivityInMemoryRepository : ActivityRepository {
 
@@ -41,20 +38,5 @@ internal class ActivityInMemoryRepository : ActivityRepository {
                 ) <= maxMetersDiff
             }
             .toSet()
-    }
-
-    private fun haversineDistance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
-        val earthRadius = 6371000.0 // meters
-
-        val dLat = Math.toRadians(lat2 - lat1)
-        val dLon = Math.toRadians(lon2 - lon1)
-
-        val a = sin(dLat / 2) * sin(dLat / 2) +
-                cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) *
-                sin(dLon / 2) * sin(dLon / 2)
-
-        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-        return earthRadius * c
     }
 }
